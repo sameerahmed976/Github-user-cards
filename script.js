efe6ed851caf71dcf49749c6f150ef41d8896c46
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2022-09-12 21:26:19 +0530

    added css style

--- script.js
+++ script.js
@@ -5,18 +5,20 @@ const url = "https://api.github.com/users/john-smilga/followers?per_page=100";
 const cardContainer = getElement(".cards-container");
 const getFollowers = async () => {
   const data = await getFetch(url);
-  //   console.log(data);
+  console.log(data);
   displayCard(data);
 };
 
 const displayCard = (data) => {
-  const htmlData = data.map(({ avatar_url: avatar, login, url }) => {
-    return `   <article class="card">
+  const htmlData = data
+    .map(({ avatar_url: avatar, login, html_url: url }) => {
+      return `   <article class="card">
         <img src=${avatar}alt="user" class="card-img" />
         <h2 class="card-title">${login}</h2>
         <a href=${url} class="card-url">view profile</a>
         </article>`;
-  });
+    })
+    .join("");
   cardContainer.insertAdjacentHTML("beforeend", htmlData);
 };
 
