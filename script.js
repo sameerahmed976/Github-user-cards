e9c35d44ab79be18056e80a1eeb6cb729480b4be
mohammed sameer ahmed <sameerahmed976@gmail.com>
2022-09-13 12:38:35 +0530

    added the styles

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import { getElement } from "./src/getElement.js";
 import { getFetch } from "./src/getFetch.js";
+
+const backToTop = getElement(".back-to-top");
+const indicator = getElement(".indicator-color");
+
 const url = "https://api.github.com/users/john-smilga/followers?per_page=100";
 
 const cardContainer = getElement(".cards-container");
@@ -23,5 +27,41 @@ const displayCard = (data) => {
 };
 
 // getFollowers();
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+  // console.log(scrollTop, clientHeight, scrollHeight);
+
+  if (scrollTop > 20) {
+    backToTop.classList.add("show-to-back-top");
+    backToTop.addEventListener("click", () => {
+      document.documentElement.scrollTop = `0px`;
+    });
+  } else {
+    backToTop.classList.remove("show-to-back-top");
+  }
+
+  indicator.style.width = `${
+    (scrollTop / (scrollHeight - clientHeight)) * 100
+  }%`;
+
+  // console.log(` ${((scrollHeight - clientHeight) / scrollTop) * 100} `);
+
+  // console.log((scrollTop / (scrollHeight - clientHeight)) * 100);
+
+  //  ${
+  //     ((scrollHeight - clientHeight) / scrollTop) * 100
+  //   } %
+  // console.log(
+  //   "🚀 ~ file: script.js ~ line 35 ~ window.addEventListener ~   indicator.style.width",
+  //   indicator.style.width
+  // );
+
+  //   console.log(
+  //   "🚀 ~ file: script.js ~ line 36 ~ window.addEventListener ~ scrollTop",
+  //   scrollTop,
+  //   clientHeight
+  // );
+});
 
 window.addEventListener("load", getFollowers());
